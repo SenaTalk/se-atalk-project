52214fd6d9658214106b0528da01e234316ab136
Maximo Rodriguez <128664066+maximo1234567@users.noreply.github.com>
2023-10-24 16:40:46 -0300

    Create login.php

---
+++ src/config/login.php
@@ -0,0 +1,38 @@
+<?php
+$conexion = new mysqli("servidor", "usuario", "contrasena", "basededatos");
+
+if ($conexion->connect_error) {
+    die("Error en la conexión: " . $conexion->connect_error);
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+// Hash de la contraseña (para mayor seguridad)
+$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO usuarios (username, password) VALUES ('$username', '$password')";
+$stmt = $conexion->prepare($sql);
+
+if ($stmt) {
+    $stmt->bind_param("ss", $username, $hashed_password);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        $user_id = $conexion->insert_id; // Obtenemos el ID del usuario registrado
+        echo "Registro exitoso. Tu ID de usuario es: " . $user_id;
+        }
+
+    if ($stmt->affected_rows > 0) {
+        echo "Registro exitoso. ¡Inicia sesión ahora!";
+    } else {
+        echo "Error al registrar el usuario. Inténtalo de nuevo.";
+    }
+
+    $stmt->close();
+} else {
+    echo "Error en la preparación de la consulta.";
+}
+
+$conexion->close();
+?>
